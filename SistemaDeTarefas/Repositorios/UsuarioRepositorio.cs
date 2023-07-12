c904f413c583403019ed2687be44387f023393ae
Mariana Machado Amaral <mariengca27@gmail.com>
2023-07-12 10:11:18 -0300

    DBContext, Repositorio, Interface, Assinatura Métodos

---
+++ SistemaDeTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        Task<UsuarioModel> IUsuarioRepositorio.Adicionar(UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IUsuarioRepositorio.Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<UsuarioModel> IUsuarioRepositorio.Atualizar(UsuarioModel usuario, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<UsuarioModel> IUsuarioRepositorio.BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<UsuarioModel>> IUsuarioRepositorio.BuscarTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
